875237bb6dd959d965985714d2857fa32bd642b9
ahmetksrts <ahmet.kosretas@hotmail.com>
2024-07-16 17:30:29 +0300

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,8 +19,6 @@ const App = () => {
   const handleContinue = () => {
     if (selectedOption) {
       setStep(step + 1);
-      setSelectedOption(null); // Reset selection for next step
-      setPriceRange("350TL - 1.300TL"); // Reset price range for next step
     }
   };
 
@@ -46,11 +44,20 @@ const App = () => {
           <Button className='continue-button' onClick={handleContinue}>DEVAM</Button>
         </>
       );
-      
     } else {
       return (
         <>
-          <h3> Yeni Adım: {selectedOption}</h3>
+          <h3>{selectedOption === 'option1' ? 'Kaç metrekare halı yıkanacak?' : `Yeni Adım: ${selectedOption}`}</h3>
+          <p>{selectedOption=== 'option1' ? "Büyük oda halıları 6m2'dir ve çoğu halı 1 ila 6m2 arasındadır. En yakın seçeneği seçmen yeterli." : ''}</p>
+          {selectedOption === 'option1' && (
+            <div className='options'>
+              <Button className='option-button'>opt 1</Button>
+              <Button className='option-button'>opt 2</Button>
+              <Button className='option-button'>opt 3</Button>
+              <Button className='option-button'>opt 4</Button>
+              <Button className='option-button'>opt 5</Button>
+            </div>
+          )}
           <Button className='continue-button' onClick={handleContinue}>Sonraki Adım</Button>
         </>
       );
@@ -61,7 +68,7 @@ const App = () => {
     <div className='container'>
       <div className='modal'>
         <div className='header'>
-          <h2 className='title'>{step === 1 ? 'Halı Yıkama' : 'Yeni Adım'}</h2>
+          <h2 className='title'>Halı Yıkama</h2>
           <button className='close-button'> <i className="close icon"></i> </button>
         </div>
         <div className='price-range'>
