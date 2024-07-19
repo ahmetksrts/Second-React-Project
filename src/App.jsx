c8cb3a43fd6eaacb027ff12b2df6abcd43a674af
ahmetksrts <ahmet.kosretas@hotmail.com>
2024-07-19 14:57:50 +0300

    10th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,257 +1,104 @@
 /* App.jsx */
 // @charset "UTF-8";
-/* ------------- */
+/* -------------------------- */
 
 import React, { useState, useEffect } from 'react';
 import { Button } from 'semantic-ui-react';
 import "./App.css";
 import 'semantic-ui-css/semantic.min.css';
 import CloseModal from './CloseModal/CloseModal.jsx';
+import options from "./assets/options.json";
+import { Progress } from 'semantic-ui-react'
 
 const App = () => {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [priceRange, setPriceRange] = useState("350TL - 1.300TL");
   const [step, setStep] = useState(1);
   const [isCloseModalOpen, setIsCloseModalOpen] = useState(false);
-  const [selectedStep2Option, setSelectedStep2Option] = useState(null); // Yeni state
-  const [canContinue, setCanContinue] = useState(false); // Yeni state
-  const [price2Range, setPrice2Range] = useState();
-  const [selectedStep3Option, setSelectedStep3Option] = useState(null); // Yeni state
-  const [price3Range, setPrice3Range] = useState();
-
+  const [selectedStepOption, setSelectedStepOption] = useState({});
+  const [priceRange, setPriceRange] = useState("350TL - 1.300TL");
 
   useEffect(() => {
-    if (step === 1 && selectedOption) {
-      setCanContinue(true);
-    } 
-    
-    else if (step === 2 && selectedStep2Option) {
-      setCanContinue(true);
-    } 
-    
-    else {
-      setCanContinue(false);
-    }
-  }, [step, selectedOption, selectedStep2Option]);
+    setSelectedStepOption({});
+  }, [step]);
 
-  const handleOptionClick = (option, price) => {
-    setSelectedOption(option);
-    setPriceRange(price);
-  };
-
-  const handleStep2OptionClick = (option, price) => { // Yeni handle function
-    setSelectedStep2Option(option);
-    setPrice2Range(price);
-
-  };
-
-  const handleStep3OptionClick = (option, price) => { // Yeni handle function
-    setSelectedStep3Option(option);
-    setPrice3Range(price);
-
+  const handleOptionClick = (step, option) => {
+    setSelectedOption(step === 1 ? option.value : selectedOption);
+    setSelectedStepOption(prev => ({ ...prev, [step]: option.value }));
+    setPriceRange(option.price || priceRange);
   };
 
   const handleContinue = () => {
-    if (canContinue) {
+    if (selectedStepOption[step]) {
       setStep(step + 1);
     }
   };
 
   const handleBack = () => {
     if (step > 1) {
+      setSelectedStepOption(prev => {
+        const updatedOptions = { ...prev };
+        delete updatedOptions[step];
+        return updatedOptions;
+      });
+      if (step === 2) {
+        setPriceRange("350TL - 1.300TL");
+      }
       setStep(step - 1);
     }
   };
 
-  const handleClose = () => {
-    setIsCloseModalOpen(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsCloseModalOpen(false);
-  };
-
-  const getPriceRange = () => {
-    if(selectedStep2Option){
-      return(price2Range)
-    }
-    else{
-      return(priceRange)
-    }
-  }
+  const handleClose = () => setIsCloseModalOpen(true);
+  const handleCloseModal = () => setIsCloseModalOpen(false);
 
   const getStepContent = () => {
-    const content1 = (
-      <div className='options'>
-        <Button className={`option-button ${selectedStep2Option === '5' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('5', "300TL - 800TL")}>
-          5
-          {selectedStep2Option === '5' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '10' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('10', "400TL - 800TL")}>
-          10
-          {selectedOption === '10' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '15' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('15', "500TL - 1.000TL")}>
-          15
-          {selectedOption === '15' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '20' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('20', "500TL - 1.200TL")}>
-          20
-          {selectedOption === '20' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '25' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('25', "500TL - 1.400TL")}>
-          25
-          {selectedOption === '25' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '30' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('30', "560TL - 1.500TL")}>
-          30
-          {selectedOption === '30' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '40' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('40', "750TL - 1.900TL")}>
-          40
-          {selectedOption === '40' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '50' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('50', "500TL - 2.000TL")}>
-          50
-          {selectedOption === '50' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '60' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('60', "400TL - 2.100TL")}>
-          60
-          {selectedOption === '60' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedOption === '80' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('80', "420TL - 2.100TL")}>
-          80 ve daha fazla
-          {selectedOption === '80' && <i className="check icon"></i>}
-        </Button>
-        
-      </div>
-    );
+    const titles = ["Halı nerede yıkansın?", "Kaç metrekare halı yıkanacak?", "Çıkarılmasını istediğin lekeler var mı?"];
+    const descriptions = ["", "Büyük oda halıları 6m2'dir ve çoğu halı 1 ila 6m2 arasındadır. En yakın seçeneği seçmen yeterli.", ""];
 
-    const content2 = (
-      <div className='options'>
-        <Button className={`option-button ${selectedStep2Option === '10' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('10')}>
-          10
-          {selectedStep2Option === '10' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '15' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('15')}>
-          15
-          {selectedStep2Option === '15' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '20' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('20')}>
-          20
-          {selectedStep2Option === '20' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '25' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('25')}>
-          25
-          {selectedStep2Option === '25' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '30' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('30')}>
-          30
-          {selectedStep2Option === '30' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '40' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('40')}>
-          40
-          {selectedStep2Option === '40' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '50' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('50')}>
-          50
-          {selectedStep2Option === '50' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '60' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('60')}>
-          60
-          {selectedStep2Option === '60' && <i className="check icon"></i>}
-        </Button>
-        <Button className={`option-button ${selectedStep2Option === '80' ? 'on' : ''}`} onClick={() => handleStep2OptionClick('80 ve daha fazla')}>
-          80 ve daha fazla
-          {selectedStep2Option === '80' && <i className="check icon"></i>}
-        </Button>
-      </div>
-    );
+    let stepContent = {
+      title: titles[step - 1],
+      description: descriptions[step - 1],
+      options: []
+    };
 
-    const content3= (
-      <div>
-        <Button className={`option-button ${selectedStep3Option === 'Evet' ? 'on' : ''}`} onClick={() => handleStep3OptionClick('Evet', "")}>
-          Evet, çıkarılması gereken lekeler var
-          {selectedStep2Option === '10' && <i className="check icon"></i>}
-        </Button>
-      </div>
-    );
+    if (step === 2) {
+      const selectedOptionValue = selectedOption;
+      stepContent.options = options.step2[selectedOptionValue] || options.step2.other;
 
-    if (selectedOption === 'option1') {
-      return {
-        title: 'Kaç metrekare halı yıkanacak?',
-        description: "Büyük oda halıları 6m2'dir ve çoğu halı 1 ila 6m2 arasındadır. En yakın seçeneği seçmen yeterli.",
-        content: content1
-      };
-    } 
-    
-    else if (selectedOption === 'option2') {
-      return {
-        title: 'Kaç metrekare halı yıkanacak?',
-        description: "Büyük oda halıları 6m2'dir ve çoğu halı 1 ila 6m2 arasındadır. En yakın seçeneği seçmen yeterli.",
-        content: content2
-      };
-    } 
-    
-    else if (selectedOption === 'option3') {
-      return {
-        title: 'Kaç metrekare halı yıkanacak?',
-        description: 'En yakın seçeneği seçmen yeterli.',
-        content: content2
-      };
+      stepContent.options = stepContent.options.map(option => ({
+        ...option,
+        value: `${selectedOption} + ${option.value}`
+      }));
+    } else {
+      stepContent.options = options[`step${step}`];
     }
+
+    return stepContent;
   };
 
-  const getStep3Content = () => {
-    if(selectedStep2Option){
-      return{
-        title: "Çıkarılmasını istediğin lekeler var mı?"
-      }
-    }
-  }
+  const renderOptions = (stepContent) => (
+    <div className='options'>
+      {stepContent.options.map(option => (
+        <Button
+          key={option.value}
+          className={`option-button ${selectedStepOption[step] === option.value ? 'on' : ''}`}
+          onClick={() => handleOptionClick(step, option)}
+        >
+          {option.label}
+          {selectedStepOption[step] === option.value && <i className="check icon"></i>}
+        </Button>
+      ))}
+    </div>
+  );
 
   const renderContent = () => {
-    if (step === 1) {
-      return (
-        <>
-          <h3> Halı nerede yıkansın? </h3>
-          <div className='options'>
-            <Button className={`option-button ${selectedOption === 'option1' ? 'on' : ''}`} onClick={() => handleOptionClick('option1', '400TL - 1.150TL')}>
-              Adresten alınıp teslim edilsin
-              {selectedOption === 'option1' && <i className="check icon"></i>}
-            </Button>
-            <Button className={`option-button ${selectedOption === 'option2' ? 'on' : ''}`} onClick={() => handleOptionClick('option2', '550TL - 3.200TL')}>
-              Evde halı temizliği yapılsın
-              {selectedOption === 'option2' && <i className="check icon"></i>}
-            </Button>
-            <Button className={`option-button ${selectedOption === 'option3' ? 'on' : ''}`} onClick={() => handleOptionClick('option3', '1.000TL - 10.000TL')}>
-              Ofiste halı temizliği yapılsın
-              {selectedOption === 'option3' && <i className="check icon"></i>}
-            </Button>
-          </div>
-        </>
-      );
-    } 
-    
-    else if (step === 2) {
-      const stepContent = getStepContent();
-      return (
-        <>
-          <h3>{stepContent.title}</h3>
-          <p className='option-p'>{stepContent.description}</p>
-          <div>{stepContent.content}</div>
-        </>
-      );
-    }
-
-    else if (step === 3) {
-      const step3Content = getStep3Content();
-      return (
-        <>
-          <h3>{step3Content.title}</h3>
-        </>
-      );
-    }
+    const stepContent = getStepContent();
+    return (
+      <>
+        <h3>{stepContent.title}</h3>
+        <p className='option-p'>{stepContent.description}</p>
+        {renderOptions(stepContent)}
+      </>
+    );
   };
 
   return (
@@ -264,12 +111,18 @@ const App = () => {
             </button>
           )}
           <h2 className='title'>Halı Yıkama</h2>
-          <button className='close-button' onClick={handleClose}> <i className="close icon"></i> </button>
+          <button className='close-button' onClick={handleClose}><i className="close icon"></i></button>
         </div>
+
+        <div className='progressbar'>
+          <Progress percent={83} color='green' size='tiny'/>
+        </div>
+
         <div className='price-range'>
           <p>Ortalama fiyat aralığı:</p>
-          <p>{getPriceRange()}</p>
+          <p>{priceRange}</p>
         </div>
+        
         {renderContent()}
         <Button className='continue-button' onClick={handleContinue}>DEVAM</Button>
       </div>
--- src/CloseModal/CloseModal.jsx
+++ src/CloseModal/CloseModal.jsx
@@ -12,7 +12,7 @@ const CloseModal = ({ onClose }) => {
     <div className="close-modal-overlay">
       <div className="close-modal">
         <h3 className='close-modal-header'>Emin misin?</h3>
-        <p className='close-modal-p'>Birkaç soruya daha cevap vererek ücretsiz <br/>teklif alabilirsin.</p>
+        <p className='close-modal-p'>Birkaç soruya daha cevap vererek ücretsiz <br />teklif alabilirsin.</p>
         <div className="close-modal-buttons">
           <Button onClick={onClose}>Devam et</Button>
           <Button onClick={handleYesClick} id='ÇIK'>ÇIK</Button>
